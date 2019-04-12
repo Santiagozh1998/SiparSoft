a70eab25851447e26e04436a6d5332f96a5e2bca
sebastian tamayo <43457239+sebastian18t@users.noreply.github.com>
2019-04-11 19:06:26 -0500

    Revert "Sebastian18t"

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -3,9 +3,7 @@ package Vista;
 public class Main {
     
     public static void main (String args[]){
-        //new VentanaInicioSesion();
-        VentanaInicioSesion inicio = new VentanaInicioSesion("SEBASTIAN", 4545); 
-       // new  windowAdmin();     
+        new  windowAdmin();     
     }    
     
 }
